9a87017ae3ffd29614f341b9849da1cb754ab134
Joshua Hoogstraat <joshua.hoogstraat@icloud.com>
2026-03-18 23:55:26 +0100

    Use custom id for provider

--- iptv/Model/Database/Provider.swift
+++ iptv/Model/Database/Provider.swift
@@ -9,10 +9,32 @@ import Foundation
 import SwiftData
 
 @Model class Provider: Identifiable {
+    
+    @Attribute(.unique)
+    var id: UUID = UUID()
+    
+    @Attribute(.unique)
     var name: String
+    
+    var isValid: Bool { !name.isEmpty }
+    var type: String { "Generic Provider" }
     
     init(name: String) {
         self.name = name
+    }
+    
+    static func with(id: UUID, in context: ModelContext) -> Provider? {
+        var descriptor = FetchDescriptor<Provider>(predicate: #Predicate { $0.id == id } )
+        descriptor.fetchLimit = 1
+        
+        return try? context.fetch(descriptor).first
+    }
+    
+    static func with(name: String, in context: ModelContext) -> Provider? {
+        var descriptor = FetchDescriptor<Provider>(predicate: #Predicate { $0.name == name } )
+        descriptor.fetchLimit = 1
+        
+        return try? context.fetch(descriptor).first
     }
 }
 
@@ -25,9 +47,11 @@ import SwiftData
     var movies: [Movie]
     var series: [Series]
     
-    var isValid: Bool {
-         !username.isEmpty && !password.isEmpty
+    override var isValid: Bool {
+        super.isValid && !username.isEmpty && !password.isEmpty
     }
+    
+    override var type: String { "Xtream API" }
     
     init(name: String, endpoint: URL, username: String, password: String, movies: [Movie] = [], series: [Series] = []) {
         self.endpoint = endpoint
